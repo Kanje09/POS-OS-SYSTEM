7acaafaa1ea0765f81e2598a8e6813568a046d6b
Kanje <rowyneo09@gmail.com>
2026-02-13 01:29:23 +0800

    fix

--- backend/dist/config/index.js
+++ backend/dist/config/index.js
@@ -1,22 +1,26 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.config = {
-    //Server configuration
-    PORT: process.env.PORT || 5000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_HOST: process.env.DB_HOST || 'localhost',
-    DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'Yasuo890',
-    DB_NAME: process.env.DB_NAME || 'myposdb',
-    DB_PORT: parseInt(process.env.DB_PORT || '3306'),
-    JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret_key',
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '24h',
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY || '',
-    CORS_ORIGINS: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : ['http://localhost:5000'],
+  //Server configuration
+  PORT: process.env.PORT || 5000,
+  NODE_ENV: process.env.NODE_ENV || "production",
+  DB_HOST: process.env.DB_HOST || "maglev.proxy.rlwy.net",
+  DB_USER: process.env.DB_USER || "root",
+  DB_PASSWORD: process.env.DB_PASSWORD || "LzdDvqNELPxaknLmsydfriCOyOkCntMz",
+  DB_NAME: process.env.DB_NAME || "myposdb",
+  DB_PORT: parseInt(process.env.DB_PORT || "28153"),
+  JWT_SECRET: process.env.JWT_SECRET || "your_jwt_secret_key",
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "24h",
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY || "",
+  CORS_ORIGIN: process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(",")
+    : ["http://localhost:5000"],
 };
