50b660eb7eaa83b985ed71ec7f9f735446c026aa
Kanje <rowyneo09@gmail.com>
2026-02-13 01:22:41 +0800

    fix

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -5,18 +5,18 @@ dotenv.config();
 export const config = {
     //Server configuration
     PORT: process.env.PORT || 5000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
 
-    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_HOST: process.env.DB_HOST || 'maglev.proxy.rlwy.net',
     DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'Yasuo890',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'LzdDvqNELPxaknLmsydfriCOyOkCntMz',
     DB_NAME: process.env.DB_NAME || 'myposdb',
-    DB_PORT: parseInt(process.env.DB_PORT || '3306'),
+    DB_PORT: parseInt(process.env.DB_PORT || '28153'),
 
     JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret_key',
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '24h',
 
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY || '',
 
-    CORS_ORIGINS: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : ['http://localhost:5000'],
+    CORS_ORIGIN: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5000'],
 };
