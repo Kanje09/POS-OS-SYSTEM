579df91fa3ed3b2d254c6c93c843f6d9c1353d8d
Kanje <rowyneo09@gmail.com>
2026-02-13 00:17:47 +0800

    try fix - deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,4 @@
 import app from "./app";
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
