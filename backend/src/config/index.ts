1495e1ed84ff485f7eae7a4f01a2558a9e6b12e1
Kanje <rowyneo09@gmail.com>
2026-02-13 04:39:22 +0800

    fixx

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -7,11 +7,11 @@ export const config = {
     PORT: process.env.PORT || 5000,
     NODE_ENV: process.env.NODE_ENV || 'production',
 
-    DB_HOST: process.env.DB_HOST || 'maglev.proxy.rlwy.net',
+    DB_HOST: process.env.DB_HOST || 'mysql-production-670a.up.railway.app',
     DB_USER: process.env.DB_USER || 'root',
     DB_PASSWORD: process.env.DB_PASSWORD || 'LzdDvqNELPxaknLmsydfriCOyOkCntMz',
     DB_NAME: process.env.DB_NAME || 'myposdb',
-    DB_PORT: parseInt(process.env.DB_PORT || '28153'),
+    DB_PORT: parseInt(process.env.DB_PORT || '3306'),
 
     JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret_key',
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '24h',
